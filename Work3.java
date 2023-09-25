7ab022fb27a64bdaf8a7db30eac124b9ec3cd772
G00436695 ATU <G00436695@atu.ie>
2023-09-25 10:24:34 +0000

    new file

---
+++ Work3.java
@@ -0,0 +1,8 @@
+public class Work3{ //work is the file name / class
+
+    public static void main(String[] args){ //this is the main method
+            System.out.print("Welcome\nto\nwork\nfile\n1\n");
+
+    } //this ends the method (2)
+
+}// this ends the class (1)
