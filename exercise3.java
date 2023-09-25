d4298b6e4b7d24ed1ef2cd6c8dd99e5f3db6af13
G00436695 ATU <G00436695@atu.ie>
2023-09-25 10:30:02 +0000

    new file

---
+++ exercise3.java
@@ -0,0 +1,9 @@
+public class exercise3{ 
+
+    public static void main(String[] args){
+            System.out.print("Welcome to Java Programming! ");
+            System.out.println("\nmy name is shaun");
+
+    } 
+
+}
