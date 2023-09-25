58a5e3b1d16ac32526ef2aabf6ff1298f1cef6f3
G00436695 ATU <G00436695@atu.ie>
2023-09-25 10:35:14 +0000

    multiple

---
+++ exercises/exercise5.java
@@ -0,0 +1,11 @@
+public class exercise5{ 
+
+    public static void main(String[] args){
+            System.out.print("Welcome to Java Programming! ");
+            System.out.print("\nmy name is shaun");
+            System.out.print("\ni belong to gmit");
+            System.out.println("\nfor now anyways");
+
+    } 
+
+}
---
+++ exercises/exercise6.java
@@ -0,0 +1,11 @@
+public class exercise5{ 
+
+    public static void main(String[] args){
+            System.out.print("Welcome to Java Programming! ");
+            System.out.print("\nmy name is shaun");
+            System.out.print("\ni belong to gmit");
+            System.out.print("\nfor now anyways");
+            System.out.println("\nwhen i leave ill get a job and be loaded");
+    } 
+
+}
