0b34b57580625267761e88b82aea9de15d08d24c
G00436695 ATU <G00436695@atu.ie>
2023-09-25 10:15:58 +0000

    fix

---
+++ Work2.java
@@ -0,0 +1,9 @@
+public class Work2{ //work is the file name / class
+
+    public static void main(String[] args){ //this is the main method
+            System.out.println("Welcome to work file 1");
+            System.out.print("Lines");
+
+    } //this ends the method (2)
+
+}// this ends the class (1)
